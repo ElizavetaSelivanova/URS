cf251cbbf18ec3bb9eff5b427cabd87cbe635a62
IrinaLevina <irina.levina.i@gmail.com>
2014-10-17 22:29:06 +0300

    New files

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Liza on 17.10.2014.
+ */
+ //IRA's CODE BEGIN
+$(document).ready(function(){
+	var moveBase = [
+		//position layout [Left Hand, Right Hand, Left Leg, Right Leg] 1 - if active
+		{id: 1, name: "LHandUp", functionName: "", x:"", y:"", position: [1,0,0,0]},
+		{id: 2, name: "RHandUp", functionName: "", x:"", y:"", position: [0,1,0,0]},
+	];
+	var humanPosition {position: [0,0,0,0], moveActiveId: ""};
+
+})
+//IRA's CODE END
