45829eab6ea4ef004fa36e40228326bae5386be3
IrinaLevina <irina.levina.i@gmail.com>
2014-10-19 11:08:13 +0300

    new changes in all files

--- index.js
+++ index.js
@@ -4,24 +4,66 @@
  //IRA's CODE BEGIN
  var moveBase = [
 		//position layout [Left Hand, Right Hand, Left Leg, Right Leg] 1 - if active
-		{id: 1, name: "lHandUp", functionName: "", x:"175", y:"95",time: "0,3", position: [1,0,0,0]},
-		{id: 2, name: "rHandUp", functionName: "", x:"5", y:"85", time: "0,2", position: [0,1,0,0]},
-		{id: 3, name: "lLegToSide", functionName: "", x:"15", y:"95", time:"0,1", position: [0,0,1,0]},
-		{id: 4, name: "rLegToSide", functionName: "", x:"165", y:"85", time: "0,3", position: [0,0,0,1]},
-		{id: 5, name: "lHandToSide", functionName: "", x:"210", y:"95", time:"0,2", position: [1,0,0,0]},
-		{id: 6, name: "rHandToSide", functionName: "", x:"-25", y:"85",time:"0,3", position: [0,1,0,0]},
-        {id: 7, name: "lHandUp", functionName: "", x:"195", y:"95",time: "0,3", position: [1,0,0,0]},
-		{id: 8, name: "rHandUp", functionName: "", x:"-10", y:"85", time: "0,2", position: [0,1,0,0]},
-		{id: 9, name: "lLegToSide", functionName: "", x:"25", y:"95", time:"0,1", position: [0,0,1,0]},
-		{id: 10, name: "rLegToSide", functionName: "", x:"135", y:"85", time: "0,3", position: [0,0,0,1]},
-		{id: 11, name: "lHandToSide", functionName: "", x:"230", y:"95", time:"0,2", position: [1,0,0,0]},
-		{id: 12, name: "rHandToSide", functionName: "", x:"-45", y:"85",time:"0,3", position: [0,1,0,0]},
-        {id: 13, name: "lHandUp", functionName: "", x:"205", y:"95",time: "0,3", position: [1,0,0,0]},
-		{id: 14, name: "rHandUp", functionName: "", x:"45", y:"85", time: "0,2", position: [0,1,0,0]},
-		{id: 15, name: "lLegToSide", functionName: "", x:"55", y:"95", time:"0,1", position: [0,0,1,0]},
-		{id: 16, name: "rLegToSide", functionName: "", x:"195", y:"85", time: "0,3", position: [0,0,0,1]},
-		{id: 17, name: "lHandToSide", functionName: "", x:"220", y:"95", time:"0,2", position: [1,0,0,0]},
-		{id: 18, name: "rHandToSide", functionName: "", x:"-65", y:"85",time:"0,3", position: [0,1,0,0]}
+		{id: 1, name: "lHandUp", functionName: "", x:"145", y:"95",time: "0,3", position: [1,0,0,0]},
+		{id: 2, name: "lHandUp", functionName: "", x:"155", y:"95",time: "0,3", position: [1,0,0,0]},
+		{id: 3, name: "lHandUp", functionName: "", x:"165", y:"95",time: "0,3", position: [1,0,0,0]},
+		{id: 4, name: "lHandUp", functionName: "", x:"175", y:"95",time: "0,3", position: [1,0,0,0]},
+		{id: 5, name: "lHandUp", functionName: "", x:"185", y:"95",time: "0,3", position: [1,0,0,0]},
+		{id: 6, name: "lHandUp", functionName: "", x:"195", y:"95",time: "0,3", position: [1,0,0,0]},
+		{id: 7, name: "lHandUp", functionName: "", x:"205", y:"95",time: "0,3", position: [1,0,0,0]},
+		{id: 8, name: "lHandUp", functionName: "", x:"215", y:"95",time: "0,3", position: [1,0,0,0]},
+		{id: 9, name: "lHandUp", functionName: "", x:"225", y:"95",time: "0,3", position: [1,0,0,0]},
+		{id: 10, name: "lHandUp", functionName: "", x:"175", y:"95",time: "0,3", position: [1,0,0,0]},
+		{id: 11, name: "rHandUp", functionName: "", x:"5", y:"85", time: "0,2", position: [0,1,0,0]},
+		{id: 12, name: "rHandUp", functionName: "", x:"15", y:"85", time: "0,2", position: [0,1,0,0]},
+		{id: 13, name: "rHandUp", functionName: "", x:"25", y:"85", time: "0,2", position: [0,1,0,0]},
+		{id: 14, name: "rHandUp", functionName: "", x:"35", y:"85", time: "0,2", position: [0,1,0,0]},
+		{id: 15, name: "rHandUp", functionName: "", x:"45", y:"85", time: "0,2", position: [0,1,0,0]},
+		{id: 16, name: "rHandUp", functionName: "", x:"55", y:"85", time: "0,2", position: [0,1,0,0]},
+		{id: 17, name: "rHandUp", functionName: "", x:"65", y:"85", time: "0,2", position: [0,1,0,0]},
+		{id: 18, name: "rHandUp", functionName: "", x:"75", y:"85", time: "0,2", position: [0,1,0,0]},
+		{id: 19, name: "rHandUp", functionName: "", x:"85", y:"85", time: "0,2", position: [0,1,0,0]},
+		{id: 20, name: "rHandUp", functionName: "", x:"95", y:"85", time: "0,2", position: [0,1,0,0]},
+		{id: 21, name: "lLegToSide", functionName: "", x:"5", y:"95", time:"0,1", position: [0,0,1,0]},
+		{id: 22, name: "lLegToSide", functionName: "", x:"15", y:"95", time:"0,1", position: [0,0,1,0]},
+		{id: 23, name: "lLegToSide", functionName: "", x:"25", y:"95", time:"0,1", position: [0,0,1,0]},
+		{id: 24, name: "lLegToSide", functionName: "", x:"35", y:"95", time:"0,1", position: [0,0,1,0]},
+		{id: 25, name: "lLegToSide", functionName: "", x:"45", y:"95", time:"0,1", position: [0,0,1,0]},
+		{id: 26, name: "lLegToSide", functionName: "", x:"55", y:"95", time:"0,1", position: [0,0,1,0]},
+		{id: 27, name: "lLegToSide", functionName: "", x:"65", y:"95", time:"0,1", position: [0,0,1,0]},
+		{id: 28, name: "lLegToSide", functionName: "", x:"75", y:"95", time:"0,1", position: [0,0,1,0]},
+		{id: 29, name: "lLegToSide", functionName: "", x:"85", y:"95", time:"0,1", position: [0,0,1,0]},
+		{id: 30, name: "lLegToSide", functionName: "", x:"95", y:"95", time:"0,1", position: [0,0,1,0]},
+		{id: 31, name: "rLegToSide", functionName: "", x:"145", y:"85", time: "0,3", position: [0,0,0,1]},
+		{id: 32, name: "rLegToSide", functionName: "", x:"155", y:"85", time: "0,3", position: [0,0,0,1]},
+		{id: 33, name: "rLegToSide", functionName: "", x:"165", y:"85", time: "0,3", position: [0,0,0,1]},
+		{id: 34, name: "rLegToSide", functionName: "", x:"175", y:"85", time: "0,3", position: [0,0,0,1]},
+		{id: 35, name: "rLegToSide", functionName: "", x:"185", y:"85", time: "0,3", position: [0,0,0,1]},
+		{id: 36, name: "rLegToSide", functionName: "", x:"195", y:"85", time: "0,3", position: [0,0,0,1]},
+		{id: 37, name: "rLegToSide", functionName: "", x:"205", y:"85", time: "0,3", position: [0,0,0,1]},
+		{id: 38, name: "rLegToSide", functionName: "", x:"215", y:"85", time: "0,3", position: [0,0,0,1]},
+		{id: 39, name: "rLegToSide", functionName: "", x:"225", y:"85", time: "0,3", position: [0,0,0,1]},
+		{id: 40, name: "rLegToSide", functionName: "", x:"235", y:"85", time: "0,3", position: [0,0,0,1]},
+		{id: 41, name: "lHandToSide", functionName: "", x:"30", y:"95", time:"0,2", position: [1,0,0,0]},
+		{id: 42, name: "lHandToSide", functionName: "", x:"40", y:"95", time:"0,2", position: [1,0,0,0]},
+		{id: 43, name: "lHandToSide", functionName: "", x:"50", y:"95", time:"0,2", position: [1,0,0,0]},
+		{id: 44, name: "lHandToSide", functionName: "", x:"60", y:"95", time:"0,2", position: [1,0,0,0]},
+		{id: 45, name: "lHandToSide", functionName: "", x:"70", y:"95", time:"0,2", position: [1,0,0,0]},
+		{id: 46, name: "lHandToSide", functionName: "", x:"80", y:"95", time:"0,2", position: [1,0,0,0]},
+		{id: 47, name: "lHandToSide", functionName: "", x:"90", y:"95", time:"0,2", position: [1,0,0,0]},
+		{id: 48, name: "lHandToSide", functionName: "", x:"100", y:"95", time:"0,2", position: [1,0,0,0]},
+		{id: 49, name: "lHandToSide", functionName: "", x:"110", y:"95", time:"0,2", position: [1,0,0,0]},
+		{id: 50, name: "rHandToSide", functionName: "", x:"-25", y:"85",time:"0,3", position: [0,1,0,0]},
+		{id: 51, name: "rHandToSide", functionName: "", x:"-35", y:"85",time:"0,3", position: [0,1,0,0]},
+		{id: 52, name: "rHandToSide", functionName: "", x:"-45", y:"85",time:"0,3", position: [0,1,0,0]},
+		{id: 53, name: "rHandToSide", functionName: "", x:"-55", y:"85",time:"0,3", position: [0,1,0,0]},
+		{id: 54, name: "rHandToSide", functionName: "", x:"-65", y:"85",time:"0,3", position: [0,1,0,0]},
+		{id: 55, name: "rHandToSide", functionName: "", x:"-75", y:"85",time:"0,3", position: [0,1,0,0]},
+		{id: 56, name: "rHandToSide", functionName: "", x:"-85", y:"85",time:"0,3", position: [0,1,0,0]},
+		{id: 57, name: "rHandToSide", functionName: "", x:"-95", y:"85",time:"0,3", position: [0,1,0,0]},
+		{id: 58, name: "rHandToSide", functionName: "", x:"-105", y:"85",time:"0,3", position: [0,1,0,0]},
+		{id: 59, name: "rHandToSide", functionName: "", x:"-115", y:"85",time:"0,3", position: [0,1,0,0]},
+        {id: 60, name: "rHandToSide", functionName: "", x:"-125", y:"85",time:"0,3", position: [0,1,0,0]}
 /*		{id: 7, name: "mixer", functionName: "", x:"", y:"", time:"0,1",position: [1,1,1,1]}*/
 	];
     var idBodyParts = ["._right_hand","._left_hand","._right_leg","._left_leg"];//for position identifikate
@@ -31,6 +73,8 @@
 	var start;
     var end;
     var danceResult=[];
+    var danceResult2=[];
+    var chooseLevel = 0;
 $(document).ready(function(){
 	$(".beat-block").click(function(){
         if (ifRecordBeat==0){
@@ -61,6 +105,7 @@ $(document).ready(function(){
     $('.button-play_again').click(function(){
         $('.dancer-wrapper div').attr("style","")
         playDance(danceResult);
+        /*playDance(danceResult2)*/;
     });
 
     $('.button-do_again').click(function(){
@@ -76,8 +121,9 @@ $(document).ready(function(){
 
 var checkBeat = function(){
     var beatLength;
+    var checkEmpty = 0;
 	$('body').on("keydown", function(){
-        if (recordedBeatArray.length > 0 ){
+        if (checkEmpty != 0 ){
             if (end!=undefined){
                 start = end;
             }
@@ -90,8 +136,9 @@ var checkBeat = function(){
         }
         else{
             start = new Date();
-            recordedBeatArray.push(0);
+            checkEmpty = 1;
         }
+        console.log(recordedBeatArray);
     });
 }
 
@@ -123,25 +170,39 @@ function rotate(whatRotate, degree, time) {
 		'-o-transition': 'all '+ time +'s',
 		'transition': 'all '+ time +'s'
 	});
+
 }
 
 
 var generateMovesList = function(){
     var moveLength;
+    var generateNumber;
+    var generateNumber2;
     moveLength = moveBase.length;
-    recordedBeatArray.splice(0,1);
     $(recordedBeatArray).each(function(){
-        danceResult.push(Math.floor((Math.random() * (moveLength-1)) + 1));
+        generateNumber = Math.floor((Math.random() * (moveLength-1)) + 1);
+        generateNumber2 = Math.floor((Math.random() * (moveLength-1)) + 1);
+        humanPosition = moveBase[generateNumber].position;
+        danceResult.push(generateNumber);
+        /*danceResult2.push(generateNumber2);*/
     });
     playDance(danceResult);
+    /*if (chooseLevel = 1){
+        playDance(danceResult2);//tictionic level
+    }*/
+
+
 }
 
 var playDance = function(dance){
     console.log(dance);
     var whatRotateId, whatRotateElement, time;
     var timeOut;
-    var i = 0;
+    var i = -1;
+    var legActive=0;
     var displayNumber = function() {
+        console.log("22")
+        i++;
         humanPosition = moveBase[dance[i]].position;
         for (var j=0; j<=humanPosition.length;j++){
            if (humanPosition[j]==1){
@@ -155,39 +216,31 @@ var playDance = function(dance){
         }else{
             timeOut = 0;
         }
-        i++;
-        if (i < dance.length) {
+
+
+        if (i < dance.length-1) {
             if (i>0){
-                if (dance[i]==dance[i-1]){
+                /*if (dance[i]==dance[i-1]){
                     rotate(whatRotateElement, moveBase[dance[i]].y ,0);
-                    /*whatRotateElement.attr("style","");*/
+                    *//*whatRotateElement.attr("style","");*//*
+                }*/
+                if (chooseLevel !=2){//иначе- балерун
+                    if (dance[i]<40 && dance[i]>20){
+                        if (legActive != 0) {
+                           rotate($('._left_leg'), 95 ,0);
+                           rotate($('._right_leg'), 85 ,0);
+                        }else{
+                            legActive=1;
+                        }
+                    }
                 }
+
             }
 
             setTimeout(displayNumber, timeOut);
         }
+
     };
 
     displayNumber(); // to get the ball rolling
 }
-/*
-var randomNumber = function(){
-    var ifNumberTrue = 0;
-    var number;
-    var moveLength;
-    moveLength = moveBase.length;
-    number = Math.floor((Math.random() * (moveLength-1)) + 1);
-    if (humanPosition[2]==1||humanPosition[3]==1){
-        if (moveBase[number].position[2]==1||moveBase[number].position[3]==1){
-            ifNumberTrue = 1;
-            console.log("11");
-        }
-    }
-    if (ifNumberTrue != 0){
-        randomNumber();
-    }else{
-        return number;
-    }
-}
-*/
-//end by Liza
